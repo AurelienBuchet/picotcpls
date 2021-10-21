7e8fb734b469b78e01b32cca122a71f9a75c8c71
Emery Assogba <assogba.emery@gmail.com>
2021-10-21 08:56:29 +0100

    fix cmd

--- t/ipmininet/achange_cc.sh
+++ t/ipmininet/achange_cc.sh
@@ -5,4 +5,4 @@ sudo sysctl net.ipv4.tcp_congestion_control
 sudo sysctl -w net.ipv4.tcp_congestion_control=$CC
 sudo sysctl net.ipv4.tcp_congestion_control
 sudo sysctl net.ipv4.tcp_available_congestion_control
-sudo sysctl -p
+#sudo sysctl -p
